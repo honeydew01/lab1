e8467c0147ccfaa1e963a24098ae3313f1a60888
Jeremy Wei <jumboberryhoneydew@gmail.com>
2025-08-29 16:24:20 -0500

    Added parser from example.

--- assembler.c
+++ assembler.c
@@ -4,6 +4,14 @@
 #include <ctype.h>
 #include <limits.h>
 
+#define MAX_LINE_LENGTH 255
+
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4);
+
+enum{
+    DONE, OK, EMPTY_LINE
+};
+
 FILE *infile = NULL;
 FILE *outfile = NULL;
 
@@ -41,4 +49,53 @@ int main(int argc, char* argv[]) {
 
     fclose(infile);
     fclose(outfile);
+}
+
+int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4)
+{
+    char * lRet, * lPtr;
+    int i;
+    if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
+    return( DONE );
+    for( i = 0; i < strlen( pLine ); i++ )
+    pLine[i] = tolower( pLine[i] );
+    
+        /* convert entire line to lowercase */
+    *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
+
+    /* ignore the comments */
+    lPtr = pLine;
+
+    while( *lPtr != ';' && *lPtr != '\0' &&
+    *lPtr != '\n' ) 
+    lPtr++;
+
+    *lPtr = '\0';
+    if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
+    return( EMPTY_LINE );
+
+    if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+    {
+    *pLabel = lPtr;
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    }
+    
+        *pOpcode = lPtr;
+
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    
+        *pArg1 = lPtr;
+    
+        if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+    *pArg2 = lPtr;
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+    *pArg3 = lPtr;
+
+    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+
+    *pArg4 = lPtr;
+
+    return( OK );
 }
