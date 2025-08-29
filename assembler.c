89735caf1c107fa3a6a89755e12d28a375a74a3f
Jeremy Wei <jumboberryhoneydew@gmail.com>
2025-08-29 16:29:59 -0500

    added more funcs

--- assembler.c
+++ assembler.c
@@ -13,25 +13,31 @@
 
 #define MAX_LINE_LENGTH 255
 
-int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4);
+int readAndParse(FILE *pInfile, char *pLine, char **pLabel, char **pOpcode, char **pArg1, char **pArg2, char **pArg3, char **pArg4);
+int isOpcode(char *in);
+int toNum(char *pStr);
 
-enum{
-    DONE, OK, EMPTY_LINE
+enum
+{
+    DONE,
+    OK,
+    EMPTY_LINE
 };
 
 FILE *infile = NULL;
 FILE *outfile = NULL;
 
-int* labels = {"ADD", "AND", "BRn", "BRz", "BRp", "BR", "BRzp", "BRnp", "BRnz", "BRnzp", "HALT", "JMP", "JSR", "JSRR", "LDB", "LDW",
-"LEA", "NOP", "NOT", "RET", "LSHF", "RSHFL", "RSHFA", "RTI", "STB", "STW", "TRAP", "XOR"};
+// int *labels = {"ADD", "AND", "BRn", "BRz", "BRp", "BR", "BRzp", "BRnp", "BRnz", "BRnzp", "HALT", "JMP", "JSR", "JSRR", "LDB", "LDW",
+//                "LEA", "NOP", "NOT", "RET", "LSHF", "RSHFL", "RSHFA", "RTI", "STB", "STW", "TRAP", "XOR"};
 int numLabels = 25;
 
-int main(int argc, char* argv[]) {
-    char *prgName   = NULL;
+int main(int argc, char *argv[])
+{
+    char *prgName = NULL;
     char *iFileName = NULL;
     char *oFileName = NULL;
 
-    prgName   = argv[0];
+    prgName = argv[0];
     iFileName = argv[1];
     oFileName = argv[2];
 
@@ -42,67 +48,159 @@ int main(int argc, char* argv[]) {
     /* open the source file */
     infile = fopen(argv[1], "r");
     outfile = fopen(argv[2], "w");
-        
-    if (!infile) {
+
+    if (!infile)
+    {
         printf("Error: Cannot open file %s\n", argv[1]);
         exit(4);
     }
-    if (!outfile) {
+    if (!outfile)
+    {
         printf("Error: Cannot open file %s\n", argv[2]);
         exit(4);
     }
 
+    char lLine[MAX_LINE_LENGTH + 1], *lLabel, *lOpcode, *lArg1, *lArg2, *lArg3, *lArg4;
+
+    int lRet;
+
+    FILE *lInfile;
     /* Do stuff with files */
+    do
+    {
+        lRet = readAndParse(lInfile, lLine, &lLabel,
+                            &lOpcode, &lArg1, &lArg2, &lArg3, &lArg4);
+        if (lRet != DONE && lRet != EMPTY_LINE)
+        {
+            printf("%s", lLine);
+        }
+    } while (lRet != DONE);
 
     fclose(infile);
     fclose(outfile);
 }
 
-int readAndParse( FILE * pInfile, char * pLine, char ** pLabel, char ** pOpcode, char ** pArg1, char ** pArg2, char ** pArg3, char ** pArg4)
+int isOpcode(char *in)
 {
-    char * lRet, * lPtr;
+    return 0;
+}
+
+int readAndParse(FILE *pInfile, char *pLine, char **pLabel, char **pOpcode, char **pArg1, char **pArg2, char **pArg3, char **pArg4)
+{
+    char *lRet, *lPtr;
     int i;
-    if( !fgets( pLine, MAX_LINE_LENGTH, pInfile ) )
-    return( DONE );
-    for( i = 0; i < strlen( pLine ); i++ )
-    pLine[i] = tolower( pLine[i] );
-    
-        /* convert entire line to lowercase */
+    if (!fgets(pLine, MAX_LINE_LENGTH, pInfile))
+        return (DONE);
+    for (i = 0; i < strlen(pLine); i++)
+        pLine[i] = tolower(pLine[i]);
+
+    /* convert entire line to lowercase */
     *pLabel = *pOpcode = *pArg1 = *pArg2 = *pArg3 = *pArg4 = pLine + strlen(pLine);
 
     /* ignore the comments */
     lPtr = pLine;
 
-    while( *lPtr != ';' && *lPtr != '\0' &&
-    *lPtr != '\n' ) 
-    lPtr++;
+    while (*lPtr != ';' && *lPtr != '\0' &&
+           *lPtr != '\n')
+        lPtr++;
 
     *lPtr = '\0';
-    if( !(lPtr = strtok( pLine, "\t\n ," ) ) ) 
-    return( EMPTY_LINE );
+    if (!(lPtr = strtok(pLine, "\t\n ,")))
+        return (EMPTY_LINE);
 
-    if( isOpcode( lPtr ) == -1 && lPtr[0] != '.' ) /* found a label */
+    if (isOpcode(lPtr) == -1 && lPtr[0] != '.') /* found a label */
     {
-    *pLabel = lPtr;
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+        *pLabel = lPtr;
+        if (!(lPtr = strtok(NULL, "\t\n ,")))
+            return (OK);
     }
-    
-        *pOpcode = lPtr;
 
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
-    
-        *pArg1 = lPtr;
-    
-        if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    *pOpcode = lPtr;
+
+    if (!(lPtr = strtok(NULL, "\t\n ,")))
+        return (OK);
+
+    *pArg1 = lPtr;
+
+    if (!(lPtr = strtok(NULL, "\t\n ,")))
+        return (OK);
 
     *pArg2 = lPtr;
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    if (!(lPtr = strtok(NULL, "\t\n ,")))
+        return (OK);
 
     *pArg3 = lPtr;
 
-    if( !( lPtr = strtok( NULL, "\t\n ," ) ) ) return( OK );
+    if (!(lPtr = strtok(NULL, "\t\n ,")))
+        return (OK);
 
     *pArg4 = lPtr;
 
-    return( OK );
+    return (OK);
+}
+
+int toNum(char *pStr)
+{
+    char *t_ptr;
+    char *orig_pStr;
+    int t_length, k;
+    int lNum, lNeg = 0;
+    long int lNumLong;
+
+    orig_pStr = pStr;
+    if (*pStr == '#') /* decimal */
+    {
+        pStr++;
+        if (*pStr == '-') /* dec is negative */
+        {
+            lNeg = 1;
+            pStr++;
+        }
+        t_ptr = pStr;
+        t_length = strlen(t_ptr);
+        for (k = 0; k < t_length; k++)
+        {
+            if (!isdigit(*t_ptr))
+            {
+                printf("Error: invalid decimal operand, %s\n", orig_pStr);
+                exit(4);
+            }
+            t_ptr++;
+        }
+        lNum = atoi(pStr);
+        if (lNeg)
+            lNum = -lNum;
+
+        return lNum;
+    }
+    else if (*pStr == 'x') /* hex     */
+    {
+        pStr++;
+        if (*pStr == '-') /* hex is negative */
+        {
+            lNeg = 1;
+            pStr++;
+        }
+        t_ptr = pStr;
+        t_length = strlen(t_ptr);
+        for (k = 0; k < t_length; k++)
+        {
+            if (!isxdigit(*t_ptr))
+            {
+                printf("Error: invalid hex operand, %s\n", orig_pStr);
+                exit(4);
+            }
+            t_ptr++;
+        }
+        lNumLong = strtol(pStr, NULL, 16); /* convert hex string into integer */
+        lNum = (lNumLong > INT_MAX) ? INT_MAX : lNumLong;
+        if (lNeg)
+            lNum = -lNum;
+        return lNum;
+    }
+    else
+    {
+        printf("Error: invalid operand, %s\n", orig_pStr);
+        exit(4); /* This has been changed from error code 3 to error code 4, see clarification 12 */
+    }
 }
