9b154f7bc59bc50eba6428492273c6dfa60929b0
Andy Shen <ashennugget@gmail.com>
2025-09-03 01:12:09 -0500

    Tried implementing mul_byte.asm, not sure if it works

--- test_files/mul_byte.asm
+++ test_files/mul_byte.asm
@@ -2,24 +2,39 @@
 
 .ORIG	x3000
     LEA	R0,	f1_adr      ; Get factor 1 Address
-    LDB R1, R0, 0       ; Load factor 1 into R1
+    LDB R1, R0, #0      ; Load factor 1 into R1
     LEA R0, f2_adr      ; Get factor 2 Address
-    LDB R2, R0, 0       ; Load factor 2 into R2
+    LDB R2, R0, #0      ; Load factor 2 into R2
 
-    RSHFL R2, R2, 8     ; Right shift the second factor to the top byte of R2. 
-    ; We will use the MSB of R2 to detect if a digit in factor 2 is 1 or 0. (using the negative flag)
+    ; RSHFL R2, R2, 8     ; Right shift the second factor to the top byte of R2. 
+    ; Looks like we'll only be given unsigned integers
 
-loop: 
+    AND R3, R3, #0      ; Initialize product to 0
+    AND R4, R4, #0      ; Initialize overflow to 0
 
+LOOP ADD R2, R2, #0     ; For some reason, label cannot have its own line or reference assembler throws error 2
+    BRz EXIT            ; Exit if factor 2 = 0
+    AND R5, R2, #1
+    BRz CONTINUE        ; Skip addition if factor 2 LSB = 0
+    ADD R3, R3, R1      ; Add factor 1 to result
 
-
-
+CONTINUE LSHF R1, R1, #1 ; Left shift factor 1
+    RSHFL R2, R2, #1    ; Right shift factor 2
+    BR LOOP
     
+EXIT AND R5, R3, #0
+    LSHF R5, R3, #8     ; Check top byte for overflow
+    BRn NO_OVER
+    ADD R4, R4, #1      ; Indicate overflow
+    
+NO_OVER LEA R1, P_adr       ; Get product address
+    STB R3, R1, #0      ; Store product into mem
+    LEA R1, OF_adr      ; Get overflow address
+    STB R4, R1, #0      ; Store overflow into mem
 
-
-f1_adr: .FILL	x3100 ; Factor 1 Address
-f2_adr: .FILL	x3101 ; Factor 2 Address
-P_adr:  .FILL	x3102 ; Product Address
-OF_adr: .FILL	x3103 ; Overflow Address
+f1_adr .FILL	x3100   ; Factor 1 Address
+f2_adr .FILL	x3101   ; Factor 2 Address
+P_adr  .FILL	x3102   ; Product Address
+OF_adr .FILL	x3103   ; Overflow Address
 
 .END
